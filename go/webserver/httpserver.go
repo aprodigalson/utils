5c0e8b5d31376d4be2ab6acf4729701871784739
aprodigalson <1773221538@qq.com>
2022-12-06 22:43:45 +0800

    add webserver

--- go/webserver/httpserver.go
+++ go/webserver/httpserver.go
@@ -1,5 +1,14 @@
 package webserver
 
-func startHttpServer(){
-	
+import (
+	"io"
+	"net/http"
+)
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "hello world\n")
+}
+func startHttpServer() {
+	http.HandleFunc("/hello", hello)
+	http.ListenAndServe("127.0.0.1:12345", nil)
 }
